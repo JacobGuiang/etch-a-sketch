be481eb541b98998a21446c5c6e82582c1e41559
JacobGuiang <jacob.guiang1@gmail.com>
2022-06-30 22:59:31 -0500

    Refactor and comment JavaScript + clean up CSS

--- script.js
+++ script.js
@@ -2,45 +2,46 @@ const GRID_PIXELS = 800; // width and height in pixels of grid
 
 const container = document.getElementById('container');
 
-for(let i = 0; i < 16; i++) {
-  const gridRow = document.createElement('div');
-  gridRow.classList.add('grid-row');
-  for(let j = 0; j < 16; j++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('grid-square');
-    gridSquare.addEventListener("mouseover", () => {
-      gridSquare.classList.add('hovered');
-    });
-    gridRow.appendChild(gridSquare);
-  }
-  container.appendChild(gridRow);
-}
-
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-  let gridSize = parseInt(prompt('Enter number of squares per size of new grid'));
-  while(!gridSize || gridSize < 0 || gridSize > 100) {
-    let errorMsg = (!gridSize || gridSize < 0) ? 'Invalid number.' : 'Grid size cannot be greater than 100.';
-    gridSize = parseInt(prompt(`${errorMsg} Enter number of squares per size of new grid`));
-  }
-  container.replaceChildren();
+// create grid with gridSize number of squares per side
+function createGrid(gridSize) {
   for(let i = 0; i < gridSize; i++) {
     const gridRow = document.createElement('div');
     gridRow.classList.add('grid-row');
     for(let j = 0; j < gridSize; j++) {
       const gridSquare = document.createElement('div');
       gridSquare.classList.add('grid-square');
+      
+      // adjust width and height of each grid square
+      let squareSize = GRID_PIXELS/gridSize;
+      gridSquare.style.width = `${squareSize}px`;
+      gridSquare.style.height = `${squareSize}px`;
+
+      // add hover functionality to grid square
+      gridSquare.addEventListener('mouseover', () => {
+        gridSquare.classList.add('hovered');
+      });
+
       gridRow.appendChild(gridSquare);
     }
     container.appendChild(gridRow);
   }
-  const gridSquares = document.getElementsByClassName('grid-square');
-  let squareSize = GRID_PIXELS/gridSize;
-  Array.from(gridSquares).forEach((gridSquare) => {
-    gridSquare.style.width = `${squareSize}px`;
-    gridSquare.style.height = `${squareSize}px`;
-    gridSquare.addEventListener("mouseover", () => {
-      gridSquare.classList.add('hovered');
-    });
-  });
+}
+
+createGrid(16); // website starts with 16 x 16 grid
+
+const button = document.querySelector('button');
+
+// allow user to change grid size with button click
+button.addEventListener('click', () => {
+  // prompt user for new grid size
+  let gridSize = parseInt(prompt('Enter number of squares per size of new grid'));
+  // make sure user input is valid
+  while(!gridSize || gridSize < 0 || gridSize > 100) {
+    let errorMsg = (!gridSize || gridSize < 0) ? 'Invalid number.' : 'Grid size cannot be greater than 100.';
+    gridSize = parseInt(prompt(`${errorMsg} Enter number of squares per size of new grid`));
+  }
+
+  container.replaceChildren();  // remove previous grid
+
+  createGrid(gridSize); // create new grid with new size
 });
